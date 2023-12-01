72b456685947bf9ead7715f17807827d3612cb38
NojusMat <d00249674@student.dkit.ie>
2023-12-01 19:48:07 +0000

    :deciduous_tree: Created constructor for the tree

--- ADS_2023_CA2_Nojus_Matusevicius/ADS_2023_CA2_Nojus_Matusevicius.cpp
+++ ADS_2023_CA2_Nojus_Matusevicius/ADS_2023_CA2_Nojus_Matusevicius.cpp
@@ -1,6 +1,10 @@
 // ADS_2023_CA2_Nojus_Matusevicius.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <iostream>
 #include "XMLParser.h"
+#include <string>
+#include <iostream>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
